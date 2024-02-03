4db07b92f860188231b7c5c06428dbe75d198380
Malbornl <malbornl@student.21-school.ru>
2024-02-03 11:58:56 +0300

    update

--- dyn_matr_input_output.c
+++ dyn_matr_input_output.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//void sort_vertical(int **matrix, int n, int m, int **result_matrix);
-//void sort_horizontal(int **matrix, int n, int m, int **result_matrix);
-void input(int **matrix, int n, int m);
+int input(int **matrix, int n, int m);
 void output(int **matrix, int n, int m);
 
 
@@ -15,21 +13,26 @@ int main() {
     if(scanf("%d", &n) != 1 || n < 1) err = 1;
     if(scanf("%d", &m) != 1 || m < 1) err = 1;
    
-    int **matrix = malloc(n * sizeof(int*)); 
-    for (int i = 0; i < n; i++)
+    int **matrix = malloc(n * sizeof(int*));
+    if (matrix == NULL) err = 1;
+    
+    if (err == 0) 
+    for (int i = 0; i < n; i++){
         matrix[i] = malloc(m * sizeof(int));
-      //  int **result = (int**)malloc(n * m * sizeof(int));
-   
-    input(matrix, n, m);
-   // sort_vertical(matrix, n, m, result);
-   // output(result);
-   // sort_horizontal(matrix, n, m, result);
+        if (matrix[i] == NULL) err = 1;
+        }
+    if (err == 0)
+
+    {
+    err+= input(matrix, n, m);
+
     output(matrix, n, m);
      
     for (int i = 0; i < n; i++)
         free(matrix[i]);
     
     free (matrix); 
+    }
 
     if (err == 1) printf("n/a");
     return 0;
@@ -37,17 +40,17 @@ int main() {
 }
 
 
-void input(int **matrix, int n, int m){
-   // int err = 0;
+int input(int **matrix, int n, int m){
+    int err = 0;
     int i = 0;
     int j = 0;
     for (i = 0; i < n; i++) {
         for(j = 0; j < m; j++){
-        scanf("%d", &matrix[i][j]); 
-        //printf("строка %d колонка %d, значение: %d \n", i, j, matrix[i][j]); 
+        if(scanf("%d", &matrix[i][j]) == 0) err = 1; 
+               }
+        
         }
-        }
-//return err;
+return err;
 
 }
 void output(int **matrix, int n, int m){
@@ -60,6 +63,5 @@ void output(int **matrix, int n, int m){
         
 }
 
-//void sort_vertical(int **matrix, int n, int m, int **result_matrix) {}
-//void sort_horizontal(int **matrix, int n, int m, int **result_matrix) {}
+
 
